f4103f1586adf0df0d5b877b8d84975f9dab3401
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-03-15 09:07:14 +0100

    Filter searchbar feature imp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,24 +5,26 @@ import FlightDetails from './components/FlightDetails';
 import BookingForm from './components/BookingForm';
 import { Flight } from './types';
 
+
 const App: React.FC = () => {
   const [flights, setFlights] = useState<Flight[]>([]);
   const [selectedFlight, setSelectedFlight] = useState<Flight | null>(null);
   const [passengers, setPassengers] = useState([{ firstName: '', lastName: '', age: 0, nationality: ''}]);
 
   const searchFlights = (searchParams: any) => {
-    // Call backend API to search for flights with the given searchParams
+      console.log('searchParams:', searchParams);
+      // Call backend API to search for flights with the given searchParams
     // Set the returned flights to the flights state
     const dummyFlights: Flight[] = [
       {
-        arrivalDate: '2020-01-01',
+        arrivalDate: '2023-05-01',
         id: '1',
-        origin: 'London',
-        destination: 'New York',
-        departureDate: '2020-01-01',
-        duration: '10h',
+        origin: 'Amsterdam',
+        destination: 'Stockholm',
+        departureDate: '2023-06-01',
+        duration: '5h',
         price: 100,
-        currency: 'GBP',
+        currency: 'EUR',
         availableSeats: 10,
         layovers: [
             {
@@ -32,34 +34,64 @@ const App: React.FC = () => {
         ],
         },
         {
-            arrivalDate: '2020-01-01',
+            arrivalDate: '2023-06-01',
             id: '2',
-            origin: 'London',
-            destination: 'New York',
-            departureDate: '2020-01-01',
+            origin: 'Stockholm',
+            destination: 'Oslo',
+            departureDate: '2023-07-01',
             duration: '10h',
-            price: 100,
-            currency: 'GBP',
-            availableSeats: 10,
+            price: 200,
+            currency: 'EUR',
+            availableSeats: 40,
             layovers: [
                 {
                     airport: 'Paris',
                     duration: '2h',
                 },
-],
-},
+            ],
+        },
+        {
+            arrivalDate: '2023-07-01',
+            id: '3',
+            origin: 'Oslo',
+            destination: 'Stockholm',
+            departureDate: '2020-08-01',
+            duration: '10h',
+            price: 300,
+            currency: 'EUR',
+            availableSeats: 30,
+            layovers: [
+                {
+                    airport: 'Paris',
+                    duration: '4h',
+                },
+            ],
+                },
     ];
       // Filter dummy flights based on the searchParams
       const filteredFlights = dummyFlights.filter((flight) => {
           return (
-              flight.origin === searchParams.origin &&
-              flight.destination === searchParams.destination &&
+              flight.origin === searchParams.departure &&
+              flight.destination === searchParams.arrival &&
               flight.departureDate === searchParams.departureDate &&
               flight.availableSeats >= searchParams.passengers
           );
       });
+      console.log("Input Departure Date: ", searchParams.departureDate);
+      console.log("Origin in dummyFlights: ", dummyFlights[0].origin);
+      console.log("Input Passengers: ", searchParams.passengers);
+      console.log("Input Origin: ", searchParams.departure);
+      console.log("Input Destination: ", searchParams.arrival);
 
-      setFlights(filteredFlights);
+      console.log(filteredFlights);
+
+      if (filteredFlights.length === 0) {
+          // No flights match the search parameters
+          setFlights([]);
+          console.log('No flights match the search parameters');
+      } else {
+          setFlights(filteredFlights);
+      }
     };
 
   const handleFlightSelect = (flight: Flight) => {
--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -22,9 +22,15 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     const [returnDate, setReturnDate] = useState('');
     const [adults, setAdults] = useState(1);
     const [children, setChildren] = useState(0);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        if (!departure || !arrival || !departureDate) {
+            setErrorMessage('Please fill in all required fields.');
+            return;
+        }
 
         const searchParams: SearchParams = {
             departure,
@@ -93,45 +99,47 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
                         type="date"
                         value={returnDate}
                         onChange={(e) => setReturnDate(e.target.value)}
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow -outline"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     />
-    </div>
-  </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-4">
+                <div>
+                    <label htmlFor="adults" className="block text-gray-700 font-bold mb-2">
+                        Adults (12+)
+                    </label>
+                    <input
+                        id="adults"
+                        type="number"
+                        min={1}
+                        value={adults}
+                        onChange={(e) => setAdults(parseInt(e.target.value))}
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="children" className="block text-gray-700 font-bold mb-2">
+                        Children
+                    </label>
+                    <input
+                        id="children"
+                        type="number"
+                        min={0}
+                        value={children}
+                        onChange={(e) => setChildren(parseInt(e.target.value))}
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                </div>
+            </div>
 
-  <div className="grid grid-cols-2 gap-4 mt-4">
-    <div>
-      <label htmlFor="adults" className="block text-gray-700 font-bold mb-2">
-        Adults (12+)
-      </label>
-      <input
-        id="adults"
-        type="number"
-        min={1}
-        value={adults}
-        onChange={(e) => setAdults(parseInt(e.target.value))}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      />
-    </div>
-    <div>
-      <label htmlFor="children" className="block text-gray-700 font-bold mb-2">
-        Children
-      </label>
-      <input
-        id="children"
-        type="number"
-        min={0}
-        value={children}
-        onChange={(e) => setChildren(parseInt(e.target.value))}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      />
-    </div>
-  </div>
-
-  <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">
-    Search flights
-  </button>
-</form>
-);
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded"
+            >
+                Search flights
+            </button>
+        </form>
+    );
 };
 
 export default SearchBar;
